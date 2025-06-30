0e573d66636c1562bde21caa06c36baaf53a6ff8
Jarda <jardae@example.com>
2025-06-30 22:23:08 +0200

    :)

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-TODO
+# A little date with logic
+name = input("What's your name? ")
+age = int(input("And your age? "))
+
+if age >= 18:
+    print(f"Hello, {name}. You’re an adult.")
+else:
+    print(f"Hey, {name}. You’re still young, enjoy it!")
